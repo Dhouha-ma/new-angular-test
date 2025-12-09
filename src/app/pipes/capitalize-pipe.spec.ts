9617ce80734176ab45399ff0739e5e36b9446074
halseh92 <halim.allemagne@gmail.com>
2025-12-09 13:41:30 +0100

    Add unit test

--- src/app/pipes/capitalize-pipe.spec.ts
+++ src/app/pipes/capitalize-pipe.spec.ts
@@ -1,8 +1,17 @@
 import { CapitalizePipe } from './capitalize-pipe';
 
 describe('CapitalizePipe', () => {
+  const pipe = new CapitalizePipe();
+
   it('create an instance', () => {
-    const pipe = new CapitalizePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return empty string when value is null', () => {
+    expect(pipe.transform(null)).toBe('');
+  });
+
+  it('should return capitalized value', () => {
+    expect(pipe.transform('dhouha')).toBe('Dhouha');
   });
 });
--- src/app/services/payment.spec.ts
+++ src/app/services/payment.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { Payment } from './payment';
+import { PaymentData } from '../types/user.type';
 
 describe('Payment', () => {
   let service: Payment;
+  let httpMock: HttpTestingController;
+
+  const mockPaymentData: PaymentData[] = [
+    {
+      name: 'John Doe',
+      cardNumber: '123456789',
+      expiryDate: '12/25',
+      cvv: 123,
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [Payment],
+    });
+
     service = TestBed.inject(Payment);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getPaymentData', () => {
+    it('should retrieve payment data (GET)', () => {
+      service.getPaymentData().subscribe((data) => {
+        expect(data).toEqual(mockPaymentData);
+        expect(data.length).toBe(1);
+        expect(data[0].name).toBe('John Doe');
+      });
+
+      const req = httpMock.expectOne('/api/payment');
+      expect(req.request.method).toBe('GET');
+
+      req.flush(mockPaymentData);
+    });
   });
 });
